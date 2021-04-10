081566af3df78f2c552d9b68cd05b617389ad124
jmscottimd <jmscottimd@protonmail.com>
2021-04-10 16:58:20 -0400

    more add component work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState (false)
+
   const [tasks, setTasks] = useState([{
     "id": 1,
     "text": "Doctors Appointment",
@@ -22,6 +25,12 @@ const App = () => {
   "reminder": true
 }
 ])
+// add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000 + 1)
+  const newTask = { id, ...task}
+  setTasks([...tasks, newTask])
+}
 // delete task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
@@ -32,7 +41,8 @@ const toggleReminder = (id) => {
 }
   return (
     <div className='container'>
-      <Header />
+      <Header onAdd = {() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ?( <Tasks tasks={tasks} onDelete=
       {deleteTask} onToggle ={toggleReminder} /> ) : (
         'There are no tasks'
